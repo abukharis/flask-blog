ec3273bd7747fe2963ac02f16600580a4d3d8a93
abukharis <sa.abukharis@gmail.com>
2020-03-12 15:31:29 +0000

    working app

--- application/routes.py
+++ application/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, request
 from application import app, db, bcrypt
 # define routes for / & /home, this function will be called when these are accessed
 from application.models import Posts, Users
-from application.forms import PostForm, RegistrationForm, LoginForm
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_required, current_user, login_user, logout_user
 
 
@@ -26,12 +26,7 @@ def about():
 def post():
     form = PostForm()
     if form.validate_on_submit():
-        postData = Posts(
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
-            title = form.title.data,
-            content = form.content.data
-        )
+        postData = Posts(title = form.title.data, content = form.content.data, author=current_user)
 
         db.session.add(postData)
         db.session.commit()
@@ -46,11 +41,15 @@ def post():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    
+        
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hash_pw = bcrypt.generate_password_hash(form.password.data)
 
-        user = Users(email=form.email.data, password=hash_pw)
+        user = Users(first_name=form.first_name.data,last_name=form.last_name.data, email=form.email.data, password=hash_pw)
 
         db.session.add(user)
         db.session.commit()
@@ -72,9 +71,40 @@ def login():
             else:
                 return redirect(url_for('home'))
     return render_template('login.html', title='Login', form=form)
-
-
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('login'))
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name        
+        form.email.data = current_user.email        
+    return render_template('account.html', title='Account', form=form)
+
+
+@app.route("/account/delete", methods=['GET', 'POST'])
+@login_required
+def account_delete():
+    user = current_user.id
+   
+    account = Users.query.filter_by(id=user).first()
+    logout_user()
+    posts = Posts.query.filter_by(user_id=user)
+    
+    for post in posts:
+        db.session.delete(post)
+       
+    db.session.delete(account)
+    db.session.commit()
+    return redirect(url_for('home'))
+
